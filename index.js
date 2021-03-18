ca79a53fec3effd0f79a6af8426bfe9607930e59
Alphena <alphena@gmail.com>
2021-03-18 16:20:56 -0700

    notes added

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 // TODO: Include packages needed for this application
 
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+      },
+    
+];
+
+//inquire.prompt for the questions
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+
+
 
 // TODO: Create a function to initialize app
 function init() {}
