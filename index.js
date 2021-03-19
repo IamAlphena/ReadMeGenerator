b118422fd6c79158f77033842364c416be6d7968
Alphena <alphena@gmail.com>
2021-03-18 18:02:10 -0700

    questions added

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 // TODO: Include packages needed for this application
-
-
+const inquire = require('inquirer')''
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        name: 'name',
-        message: 'What is your name?',
+        name: 'title',
+        message: 'What is the title of your project?',
       },
-    
+      {
+        type: 'input',
+        name: 'overview',
+        message: 'Give a sentance or two explaining your project',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Select the appropriate licences',
+        choices: [//find licesnces
+        ]
+      },
+      {
+        type: 'input',
+        name: 'systems',
+        message: 'What systems and technology was used?',
+      },
+      {
+        type: 'input',
+        name: 'credit',
+        message: 'Who all contributed to this project, and any other credits needed?',
+      },
+      {
+        type: 'input',
+        name: 'use',
+        message: 'Explain how to use your application',
+      },
 ];
 
 //inquire.prompt for the questions
