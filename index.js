34365deab4858a9a446af43ae3f53cef0886a2b6
Alphena <alphena@gmail.com>
2021-03-18 23:09:38 -0700

    the questions do indeed get asked

--- index.js
+++ index.js
@@ -1,51 +1,65 @@
-// TODO: Include packages needed for this application
+//import needed packages
 const inquire = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+//Questions Array to be get needed data
 const questions = [
     {
         type: 'input',
         name: 'title',
         message: 'What is the title of your project?',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'overview',
         message: 'Give a sentance or two explaining your project',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'Select the appropriate licences',
-        choices: [//find licesnces
-        ]
-      },
-      {
+    },
+    //   {
+    //     type: 'list',
+    //     name: 'license',
+    //     message: 'Select the appropriate licences',
+    //     choices: [//find licesnces
+    //     ]
+    //   },
+    {
         type: 'input',
         name: 'systems',
         message: 'What systems and technology was used?',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'credit',
         message: 'Who all contributed to this project, and any other credits needed?',
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'use',
         message: 'Explain how to use your application',
-      },
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(filetype, data) { }    
+//     fs.writeFile(filetype, data,  (err) =>
+//     err ? console.log(err) : console.log('Created your README!')
+
+// )
 
 
+// function that runs it all
+function init() {
+    //using inquirer, 
+    //have our questioins go through node to collect our data
+    inquire
+        .prompt(questions)
+        //then take the data and pass through write to file
+        .then((data) => {
+            // let markdownPageContent = generateMarkdown(data);
 
-// TODO: Create a function to initialize app
-function init() {}
+            // writeToFile('README.md', markdownPageContent);
+        })
+}
 
-// Function call to initialize app
-init();
+// run function to get it started
+init()
